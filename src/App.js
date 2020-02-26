da898c621c62d34621d03348371fc2a8e2c891a8
mania125 <alsgurmaniadev@gmail.com>
2020-02-26 17:50:05 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // # Component Lifecycle
+  // - Render: componentWillMount() -> render() -> componentDidMount()
+  // - Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  
+  state = {}
+
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        genres={movie.genres} 
+        synopsis={movie.synopsis}
+        key={movie.id} 
+      />
+    })
+
+    return movies
+  }
+
+  _getMovies = async () =>{
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=download_count')  // ajax 호출
+    .then(response => response.json())  // .json() -> json변환
+    .then(json => {
+      return json.data.movies
+    })
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className={movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    )
+  }
 }
 
 export default App;

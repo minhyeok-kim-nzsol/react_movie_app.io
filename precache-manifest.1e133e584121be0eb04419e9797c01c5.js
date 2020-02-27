6e20cb4efa6cf1ea4b74af66bf496e7b695e5340
mania125 <alsgurmaniadev@gmail.com>
2020-02-27 09:49:55 +0900

    Updates

---
+++ precache-manifest.1e133e584121be0eb04419e9797c01c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0a4508c45b1b0b76993d901cf40ed3",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "2949ae02bc124ec37477",
+    "url": "/react_movie_app/static/css/main.7e186ca7.chunk.css"
+  },
+  {
+    "revision": "c856ac9da504295dd3ff",
+    "url": "/react_movie_app/static/js/2.ff231ca8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_movie_app/static/js/2.ff231ca8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2949ae02bc124ec37477",
+    "url": "/react_movie_app/static/js/main.c89ad4db.chunk.js"
+  },
+  {
+    "revision": "36d9aaa5a81821ac5b99",
+    "url": "/react_movie_app/static/js/runtime-main.a31449cd.js"
+  }
+]);
